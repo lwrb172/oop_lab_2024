f8784210f4e281fc96a129b4179a7c09be60444f
lwrb172 <lwrbielak@gmail.com>
2024-03-15 12:07:18 +0100

    zad 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 0);
+        Segment seg = new Segment(p1, p2);
+
+        System.out.println(seg.length());
     }
 }
---
+++ src/Segment.java
@@ -0,0 +1,22 @@
+public class Segment {
+    private Point p1;
+    private Point p2;
+
+
+    Segment(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double length() {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+}
